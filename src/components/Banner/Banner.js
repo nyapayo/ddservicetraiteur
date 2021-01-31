42d52148fd12dfc27c2fddaecb98a8682e8bdc51
siaka eric <siakajunior1997@gmail.com>
2021-01-31 19:10:22 +0100

    add region and service contacts

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,26 +3,47 @@ import './Banner.css';
 
 const Banner = props => {
   return (
-    <section className='w3-container'>
+    <section id='banner' className='w3-container'>
       <div className='w3-card w3-panel'>
-        <h1 className='w3-xlarge kalamFont'>Pour toutes vos cérémonies: </h1>
+        <h1 className='kalamFont'>Pour toutes vos cérémonies: </h1>
         <div className='w3-panel w3-border w3-purple'>
           <p id='ceremonies' className='w3-large kalamFont'>Mariage, Bâpteme, Messe, Enterrement, Anniversaire, Funérailles, Cocktails, Etc.</p>
         </div>
       </div>
-      <div className='w3-panel w3-card w3-padding'>
-        <a href='tel: +237-678-511-551' className='w3-small w3-left'>+237 678 511 551</a> 
-        <a href='mailto: siakajunior1997@gmail.com' className='w3-small w3-right'>E-mail</a>
+      <div className='w3-card w3-panel'>
+        <h1 className='kalamFont'>Dans la région de l'<strong className='w3-text-blue'>Ouest</strong></h1>
+        <p>Et partout ailleurs au Cameroun</p>
+      </div>
+      <div className='w3-card w3-panel'>
+        <h1 className='w3-text-blue kalamFont'>Contactez-nous directement</h1>
+        <p>
+          Telephone: <a href='tel: +237-678-511-551' className='w3-small'>+237 678 511 551</a>
+        </p>
+        <p>
+          SMS: <a href='sms: +237-678-511-551' className='w3-small'>+237 678 511 551</a>  
+        </p> 
+        <p>
+          Email: <a href='mailto: siakajunior1997@gmail.com' className='w3-small'>siakajunior1997@gmail.com</a>
+        </p>
+        <p>
+          WhasApp: <span className='w3-tag w3-green w3-small'>+237 678 511 551</span>
+        </p>
+        <p>
+          Facebook: <a href='' className='w3-small'>Link to Facebook page</a>
+        </p>
       </div>
       <div className='w3-card'>
-        <h1 className='w3-panel w3-xlarge kalamFont'>Nous vous proposons:</h1>
+        <div className='w3-container w3-blue'>
+          <h1 className='kalamFont'>Car nous vous proposons:</h1>
+        </div>
         <ul className='w3-ul kalamFont'>
           <li><a href='#plats'>Cuisine(Africaine & Européenne)</a></li>
           <li><a href='#decoration'>Décoration</a></li>
-          <li><a href='#patisserie'>Patisserie</a></li>
-          <li><a href='chaises'>Chaises</a></li>
-          <li><a href='#tentes'>Tentes</a></li>
-          <li><a href='#couverts'>Couverts</a></li>
+          <li><a href='#patisserie'>Mini-Patisserie</a></li>
+          <li><a href='#patisserie'>Jus Naturels</a></li>
+          <li><a href='chaises'>Location Chaises</a></li>
+          <li><a href='#tentes'>Location Tentes</a></li>
+          <li><a href='#couverts'>Location Couverts</a></li>
         </ul>
       </div>
     </section>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import 'w3-css/w3.css';
+import './index.css';
 
 import App from './App';
 
