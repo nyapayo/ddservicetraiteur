b106e793e93520dacbf196a4c7bef74c6c368cc7
siaka eric <siakajunior1997@gmail.com>
2021-02-01 20:02:10 +0100

    Fix the web site title and contacts component

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,10 +5,10 @@ const Contacts = props => {
   return (
     <section className='w3-card w3-panel' id='contactsSection'>
       <h1 className='kalamFont'>Contactez-nous directement:</h1>
-        Telephone: <a href='tel: +237-678-511-551' className=''>+237 678 511 551</a>
-        Email: <a href='mailto: siakajunior1997@gmail.com' className=''>siakajunior1997@gmail.com</a>
-        WhatsApp: <span >+237 678 511 551</span>
-        Facebook: <a href='' className=''>Link to Facebook page</a>
+        <p>Telephone: <a href='tel: +237-678-511-551' className=''>+237 678 511 551</a></p>
+        <p>Email: <a href='mailto: siakajunior1997@gmail.com' className=''>siakajunior1997@gmail.com</a></p>
+        <p>WhatsApp: <span >+237 678 511 551</span></p>
+        <p>Facebook: <a href='' className=''>Link to Facebook page</a></p>
     </section>
   );
 };
