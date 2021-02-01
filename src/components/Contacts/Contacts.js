05bdd5dfa4ea7788b2aa4ad449280e3474b97cf7
siaka eric <siakajunior1997@gmail.com>
2021-02-01 19:53:30 +0100

    Fix kalamFont on appHeader and other components

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -4,7 +4,7 @@ import './Contacts.css';
 const Contacts = props => {
   return (
     <section className='w3-card w3-panel' id='contactsSection'>
-      <h1 className='hindFont w3-text-blue'>Contactez-nous directement:</h1>
+      <h1 className='kalamFont'>Contactez-nous directement:</h1>
         Telephone: <a href='tel: +237-678-511-551' className=''>+237 678 511 551</a>
         Email: <a href='mailto: siakajunior1997@gmail.com' className=''>siakajunior1997@gmail.com</a>
         WhatsApp: <span >+237 678 511 551</span>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,17 @@ const Header = (props) => {
   return (
 		<header id='appHeader'>
 			<div className='w3-card w3-animate-top' id='appHeaderPresentation'>
-				<h1 className='w3-text-purple hindFont' id='name'>&#127799; &#127804; <strong>D&D Service-Traiteur</strong> &#128280; &#127860;</h1>
+				<h1 className='w3-text-purple' id='name'>
+					&#127799; &#127804; 
+					<strong className='kalamFont'>
+						D&D Service-Traiteur
+					</strong> 
+					&#128280; &#127860;
+				</h1>
 				<p id='slogan'>
-					<i className='w3-small'><b>Vous satisfaire est notre plaisir!</b> </i>  
+					<i className='kalamFont'>
+						Vous satisfaire est notre plaisir!
+					</i>  
 					&#x1F609;
 				</p>
 			</div>
